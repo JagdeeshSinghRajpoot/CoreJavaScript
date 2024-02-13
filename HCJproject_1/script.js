756a70280c04949adc3d705c349b112f36b63aac
JagdeeshSinghRajpoot <jagdeeshrajpoot9@gmail.com>
2024-02-13 23:11:17 +0530

    Add HCJ connection porject and DOM

---
+++ HCJproject_1/script.js
@@ -0,0 +1,29 @@
+console.log("hello");
+
+// console.dir(document.body);
+
+// let hading = document.getElementById("hadding");
+// console.dir(hading);
+
+
+// let headings = document.getElementsByClassName("CHading");
+// console.dir(headings);
+// console.log(headings);
+
+// let pera = document.getElementsByTagName("p");
+// console.log(pera); 
+
+
+let element = document.querySelector("p");
+console.log(element);
+
+
+let allElement = document.querySelectorAll("p");
+console.log(allElement);
+
+let hading = document.querySelector("#hadding");
+console.dir(hading);
+
+let perag = document.querySelector(".CHading");
+console.dir(perag);
+
